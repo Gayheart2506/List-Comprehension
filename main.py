b06af0a5a19651b8dfb280e4d9d7ffebce0b028d
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2023-10-11 21:36:12 +0000

    added a new line of list comprehension

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ name = "Gayheart"
 new_list = [letter.upper() for letter in name]
 print(new_list)
 
+l_name = "jessica"
+new_last = [names.capitalize() for names in l_name ]
+print(new_last)
+
 numbers = [1, 2, 3, 4, 5]
 multiple = [num*2 for num in numbers]
 print(multiple)
